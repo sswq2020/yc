2f685afa1e2149bbee9236d8ee3e596f33a06ef1
sswq2020 <a309108243@qq.com>
2019-06-21 16:31:07 +0800

    基础信息路由搭建

--- src/router.js
+++ src/router.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+const CommodityManage = () => import('@/views/basicManage/commodityManage/index')
+const MaterialManage = () => import('@/views/basicManage/materialManage/index')
+const PilePositionManage = () => import('@/views/basicManage/pilePositionManage/index')
+const OriginPlaceManage = () => import('@/views/basicManage/originPlaceManage/index')
+const ProductNameManage = () => import('@/views/basicManage/productNameManage/index')
+const SettlementStorageManage = () => import('@/views/basicManage/settlementStorageManage/index')
 const ShipperManage = () => import('@/views/basicManage/shipperManage/index')
+const SpecificationManage = () => import('@/views/basicManage/specificationManage/index')
 
 Vue.use(Router)
 
@@ -28,10 +35,46 @@ let commonRoutes = {
             component: () => import(/* webpackChunkName: "avatar" */ '@/views/personal/index'),
         },
         {
+            path: '/web/settlement/pageList/commodityManage',
+            name: 'commodityManage',
+            component: CommodityManage
+        },
+        {
+            path: '/web/settlement/pageList/materialManage',
+            name: 'materialManage',
+            component: MaterialManage
+        },
+        {
+            path: '/web/settlement/pageList/pilePositionManage',
+            name: 'pilePositionManage',
+            component: PilePositionManage
+        },
+        {
+            path: '/web/settlement/pageList/originPlaceManage',
+            name: 'originPlaceManage',
+            component: OriginPlaceManage
+        },
+        {
+            path: '/web/settlement/pageList/productNameManage',
+            name: 'productNameManage',
+            component: ProductNameManage
+        },  
+        {
+            path: '/web/settlement/pageList/settlementStorageManage',
+            name: 'settlementStorageManage',
+            component: SettlementStorageManage
+        },
+        {
             path: '/web/settlement/pageList/shipperManage',
             name: 'shipperManage',
             component: ShipperManage
-        }
+        },
+        {
+            path: '/web/settlement/pageList/specificationManage',
+            name: 'specificationManage',
+            component: SpecificationManage
+        }                                    
+
     ]
 }
 
---
+++ src/views/basicManage/specificationManage/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container single-page">
+    规格管理
+  </div>
+</template>
+
+<script>
+export default {
+  name: "specificationManage",
+};
+</script>
+
+<style scoped lang="less">
+.search-box {
+  flex-wrap: wrap;
+  margin: 0;
+  .form-item {
+    padding-top: 20px;
+    .el-button {
+      margin-top: 17px;
+    }
+  }
+}
+</style>
