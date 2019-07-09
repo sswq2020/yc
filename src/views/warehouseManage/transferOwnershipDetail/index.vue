3e1b8f476cfc41bd8d31a0ccb221b63ae09eb251
sswq2020 <a309108243@qq.com>
2019-07-09 09:33:25 +0800

    过户明细mock

--- src/views/warehouseManage/transferOwnershipDetail/index.vue
+++ src/views/warehouseManage/transferOwnershipDetail/index.vue
@@ -1,12 +1,334 @@
 <template>
   <div class="container single-page">
-    过户明细
+    <hlBreadcrumb :data="breadTitle"></hlBreadcrumb>
+    <div class="search-box">
+      <div class="form-item">
+        <label>新货主</label>
+        <div class="form-control">
+          <el-select v-model="form.newShipperId" placeholder="请选择" size="small">
+            <el-option
+              v-for="(item,index) in ShipperList"
+              :key="index"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </div>
+      </div>
+      <div class="form-item">
+        <label>仓库</label>
+        <div class="form-control">
+          <el-select v-model="form.deliveryStoreId" placeholder="请选择" size="small">
+            <el-option
+              v-for="(item,index) in storageList"
+              :key="index"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </div>
+      </div>
+      <div class="form-item">
+        <label>品名</label>
+        <div class="form-control">
+          <el-select v-model="form.productNameId" placeholder="请选择" size="small">
+            <el-option
+              v-for="(item,index) in ProductNameList"
+              :key="index"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </div>
+      </div>
+      <div class="form-item">
+        <label>材质</label>
+        <div class="form-control">
+          <el-select v-model="form.materialId" placeholder="请选择" size="small">
+            <el-option
+              v-for="(item,index) in MaterialList"
+              :key="index"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </div>
+      </div>
+      <div class="form-item">
+        <label>规格</label>
+        <div class="form-control">
+          <el-select v-model="form.specificationsId" placeholder="请选择" size="small">
+            <el-option
+              v-for="(item,index) in SpecificationList"
+              :key="index"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </div>
+      </div>
+      <div class="form-item">
+        <label>产地</label>
+        <div class="form-control">
+          <el-select v-model="form.originPlaceId" placeholder="请选择" size="small">
+            <el-option
+              v-for="(item,index) in OriginPlaceList"
+              :key="index"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </div>
+      </div>
+      <div class="form-item">
+        <el-button
+          type="primary"
+          :loading="isListDataLoading"
+          @click="getListDataBylistParams"
+          size="small"
+        >查询</el-button>
+        <el-button size="small" @click="clearListParams">重置</el-button>
+      </div>
+    </div>
+    <heltable
+      ref="tb"
+      @pageChange="changePage"
+      :total="listData.paginator.totalCount"
+      :currentPage="listParams.page"
+      :pageSize="listParams.pageSize"
+      :pageSizes="[20]"
+      :data="listData.list"
+      :multiple="true"
+      :loading="isListDataLoading"
+    >
+      <el-table-column
+        align="center"
+        :prop="item.prop"
+        :label="item.label"
+        :key="item.id"
+        v-for="(item) in tableHeader"
+        :show-overflow-tooltip="showOverflowTooltip"
+      >
+        <template slot-scope="scope">
+          <span>{{listData.list[scope.$index][item.prop]}}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column label="操作" fixed="right" width="60px" align="center">
+        <template slot-scope="scope">
+          <el-button type="text" @click="detail(listData.list[scope.$index])">查看明细</el-button>
+        </template>
+      </el-table-column>
+    </heltable>
   </div>
 </template>
 
 <script>
+// import NP from "number-precision";
+import { mapGetters } from "vuex";
+import { baseMixin } from "@/common/mixin.js";
+// import { judgeAuth } from "@/util/util.js";
+import _ from "lodash";
+import Dict from "@/util/dict.js";
+import heltable from "@/components/hl_table";
+import hlBreadcrumb from "@/components/hl-breadcrumb";
+
+
+const defaultFormData = {
+  newShipperId: null,
+  deliveryStoreId: null,
+  productNameId: null,
+  materialId: null,
+  specificationsId: null,
+  originPlaceId: null
+};
+const defaultListParams = {
+  pageSize: 20,
+  page: 1
+};
+const defaultListData = {
+  paginator: {
+    totalCount: 0,
+    totalPage: 1
+  },
+  list: []
+};
+const defaulttableHeader = [
+  {
+    prop: "deliveryStore",
+    label: "仓库",
+    width: "180"
+  },
+  {
+    prop: "originalShipperName",
+    label: "原货主",
+    width: "180"
+  },
+  {
+    prop: "newShipperName",
+    label: "新货主",
+    width: "180"
+  },
+  {
+    prop: "transferTime",
+    label: "过户日期",
+    width: "180"
+  },
+  {
+    prop: "pilePosition",
+    label: "区桩位",
+    width: "180"
+  },
+  {
+    prop: "piles",
+    label: "层数",
+    width: "180"
+  },
+  {
+    prop: "productName",
+    label: "品名",
+    width: "180"
+  },
+  {
+    prop: "materialName",
+    label: "材质",
+    width: "180"
+  },
+  {
+    prop: "specificationsName",
+    label: "规格",
+    width: "180"
+  },
+  {
+    prop: "originPlaceName",
+    label: "产地",
+    width: "180"
+  },
+  {
+    prop: "transferNums",
+    label: "过户数量",
+    width: "180"
+  },
+  {
+    prop: "realTransferNums",
+    label: "实际过户数量",
+    width: "180"
+  },
+  {
+    prop: "numUnit",
+    label: "数量单位",
+    width: "180"
+  },
+  {
+    prop: "transferWeights",
+    label: "过户重量",
+    width: "180"
+  },
+  {
+    prop: "realTransferWeights",
+    label: "实际过户重量",
+    width: "180"
+  },
+  {
+    prop: "weightUnit",
+    label: "重量单位",
+    width: "180"
+  },
+  {
+    prop: "measuring",
+    label: "计量方式",
+    width: "180"
+  },
+  {
+    prop: "incomingType",
+    label: "入库类型",
+    width: "180"
+  },
+  {
+    prop: "incomingId",
+    label: "入库单号",
+    width: "180"
+  },
+  {
+    prop: "transferType",
+    label: "业务类型",
+    width: "180"
+  }
+];
 export default {
-  name: "outerStorageDetail",
+  name: "transferOwnershipDetail",
+  mixins: [baseMixin],
+  components: {
+    heltable,
+    hlBreadcrumb,
+  },
+  data() {
+    return {
+      breadTitle: ["仓储管理", "过户明细"],
+      isListDataLoading: false,
+      listParams: { ...defaultListParams }, // 页数
+      form: { ...defaultFormData }, // 查询参数
+      listData: { ...defaultListData }, // 返回list的数据结构
+      tableHeader: defaulttableHeader,
+      showOverflowTooltip: true,
+      visible:false
+    };
+  },
+  computed: {
+    ...mapGetters("app", ["role", "userId", "username", "IS_SHIPPER"])
+  },
+  methods: {
+    _filter() {
+      return _.clone(Object.assign({}, this.form, this.listParams));
+    },
+    clearListParams() {
+      this.form = { ...defaultFormData };
+      this.listParams = { ...defaultListParams };
+      this.listData = { ...defaultListData };
+      this.getListData();
+    },
+    changePage(page) {
+      this.listParams.page = page;
+      this.getListData();
+    },
+    getListDataBylistParams() {
+      this.listParams = { ...defaultListParams };
+      this.getListData();
+    },
+    async getListData() {
+      let obj = this._filter();
+      this.isListDataLoading = true;
+      const res = await this.$api.getTransferDetailList(obj);
+      this.isListDataLoading = false;
+      switch (res.code) {
+        case Dict.SUCCESS:
+          this.listData = res.data;
+          break;
+        default:
+          this.listData = { ...defaultListData };
+          this.$messageError(res.errMsg);
+          break;
+      }
+    },
+    detail(item) {
+      this.visible = true;
+      console.log(item);
+    },
+    print(){
+
+    },
+    init() {
+      setTimeout(() => {
+        this.clearListParams();
+        this.perm();
+      }, 20);
+      this.perm();
+    },
+    perm() {}
+  },
+  mounted() {
+    this.init();
+  }
 };
 </script>
 
