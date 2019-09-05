f082d9383f39e4c20c5b3bc80025c98e906b3bfc
shiswq@hletong.com <shiswq@hletong.com>
2019-09-05 17:50:23 +0800

    feat: 🎸 inventoryManage store add productType state and mutatio

--- src/store/modules/warehouseManage/inventoryManage/index.js
+++ src/store/modules/warehouseManage/inventoryManage/index.js
@@ -1,9 +1,11 @@
+import Dict from 'util/dict.js'
 const store = {
     namespaced: true,
     state: {
         transferOwnership:[], // 过户
         checkout:[], // 出库申请
-        findDetail:null //查看明细
+        findDetail:null, //查看明细
+        productType:Dict.PRODUCT_OIL // 默认进来的是石油
     },
     mutations: {
         setTransferOwnership(state,payload) {
@@ -14,6 +16,9 @@ const store = {
         },
         setFindDetail(state,payload){
             state.findDetail = payload;
+        },
+        setProductType(state,payload){
+            state.productType=payload;
         }
     },
     actions: {
