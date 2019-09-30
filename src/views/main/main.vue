0f73dfef230dd58c15a50eea3b9aea93bdbd6099
xuhui <xuh@hletong.com>
2019-09-30 08:12:33 +0800

    修改hletong-ui引入方式，添加new_common.css

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,5 +1,5 @@
 <template>
-	<HletongLayout v-on:loginOut="loginOut">
+	<HletongLayout v-on:loginOut="loginOut" systemId="180283809692909568">
 		<router-view></router-view>
 	</HletongLayout>
 </template>
@@ -7,6 +7,12 @@
 <script>
 	export default {
 		name:'Main',
+		watch: {
+			$route(to,from){
+				// console.log(to);
+				this.$refs.hlLayout.getUrl()
+			}
+		},
 		methods:{
 			loginOut(){
 				if(process.env.NODE_ENV == 'development'){
--- vue.config.js
+++ vue.config.js
@@ -13,8 +13,8 @@ module.exports = {
             '/api': {
                 // target: 'https://beidou.hletong.com/',
                 //  target: 'http://192.168.4.16:25084/',// 周杨
-                //  target: 'http://192.168.4.16:25091/',// 佘慧
-                target: 'http://192.168.4.16:25092/',// 尹凯
+                 target: 'http://192.168.4.16:25091/',// 佘慧
+                // target: 'http://192.168.4.16:25092/',// 尹凯
                 // target:'http://10.1.15.119:8202/',
                 // target:'http://192.168.4.16:25083/',//liudongcai serve
                 changeOrigin: true,
@@ -29,6 +29,11 @@ module.exports = {
             }
         }
     },
+    configureWebpack: {
+		externals: {
+			hlet: 'hlet'
+		}
+	},
     lintOnSave: false,
     chainWebpack: config => {
         if (process.env.NODE_ENV === 'test') {
