2f20c460911f1e3401268cd9d1be82454cb374d1
shiswq@hletong.com <shiswq@hletong.com>
2019-10-31 15:28:44 +0800

    ci: 🎡 增加业务测试环境的静态资源替换脚本

--- appended.js
+++ appended.js
@@ -3,12 +3,16 @@ const path = require('path')
 
 const env = process.env.NODE_ENV
 console.log(`当前环境---------${env}`)
-
+let readerStream;
 if (env === "production") {
-    const readerStream = fs.createReadStream('pro_index.html');
-    const outName = path.join(__dirname, './public', 'index.html')
-    var writerStream = fs.createWriteStream(outName);
-    readerStream.pipe(writerStream);
+    readerStream = fs.createReadStream('pro_index.html');
+}else if(env=== "ywtest"){
+    readerStream = fs.createReadStream('yw_index.html');
 }
 
+if(readerStream){
+    const outName = path.join(__dirname, './public', 'index.html')
+    const writerStream = fs.createWriteStream(outName);
+    readerStream.pipe(writerStream);
+}
 
